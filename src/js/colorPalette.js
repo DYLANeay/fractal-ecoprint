9e93652818649fb03279b27b49cd02397f2d95ff
DYLANeay <dylan.eray6@gmail.com>
2025-10-29 11:29:47 +0100

    working

--- src/js/colorPalette.js
+++ src/js/colorPalette.js
@@ -1,100 +1,236 @@
 // Color Palette System with smooth gradients
 
 export const palettes = {
-    sunset: [
-        { r: 26, g: 26, b: 46 },
-        { r: 22, g: 33, b: 62 },
-        { r: 15, g: 52, b: 96 },
-        { r: 83, g: 52, b: 131 },
-        { r: 233, g: 69, b: 96 },
-        { r: 255, g: 107, b: 107 }
-    ],
-    ocean: [
-        { r: 0, g: 4, b: 40 },
-        { r: 0, g: 78, b: 146 },
-        { r: 30, g: 60, b: 114 },
-        { r: 42, g: 82, b: 152 },
-        { r: 116, g: 235, b: 213 },
-        { r: 172, g: 182, b: 229 }
-    ],
-    forest: [
-        { r: 10, g: 61, b: 12 },
-        { r: 30, g: 95, b: 31 },
-        { r: 45, g: 134, b: 48 },
-        { r: 92, g: 184, b: 92 },
-        { r: 139, g: 195, b: 74 },
-        { r: 205, g: 220, b: 57 }
-    ],
-    fire: [
-        { r: 0, g: 0, b: 0 },
-        { r: 61, g: 0, b: 0 },
-        { r: 139, g: 0, b: 0 },
-        { r: 255, g: 69, b: 0 },
-        { r: 255, g: 165, b: 0 },
-        { r: 255, g: 255, b: 0 }
-    ],
-    rainbow: [
-        { r: 255, g: 0, b: 0 },
-        { r: 255, g: 127, b: 0 },
-        { r: 255, g: 255, b: 0 },
-        { r: 0, g: 255, b: 0 },
-        { r: 0, g: 0, b: 255 },
-        { r: 75, g: 0, b: 130 },
-        { r: 148, g: 0, b: 211 }
-    ],
-    grayscale: [
-        { r: 0, g: 0, b: 0 },
-        { r: 26, g: 26, b: 26 },
-        { r: 77, g: 77, b: 77 },
-        { r: 128, g: 128, b: 128 },
-        { r: 179, g: 179, b: 179 },
-        { r: 255, g: 255, b: 255 }
-    ]
+  sunset: [
+    { r: 26, g: 26, b: 46 },
+    { r: 22, g: 33, b: 62 },
+    { r: 15, g: 52, b: 96 },
+    { r: 83, g: 52, b: 131 },
+    { r: 233, g: 69, b: 96 },
+    { r: 255, g: 107, b: 107 },
+  ],
+  ocean: [
+    { r: 0, g: 4, b: 40 },
+    { r: 0, g: 78, b: 146 },
+    { r: 30, g: 60, b: 114 },
+    { r: 42, g: 82, b: 152 },
+    { r: 116, g: 235, b: 213 },
+    { r: 172, g: 182, b: 229 },
+  ],
+  forest: [
+    { r: 10, g: 61, b: 12 },
+    { r: 30, g: 95, b: 31 },
+    { r: 45, g: 134, b: 48 },
+    { r: 92, g: 184, b: 92 },
+    { r: 139, g: 195, b: 74 },
+    { r: 205, g: 220, b: 57 },
+  ],
+  fire: [
+    { r: 0, g: 0, b: 0 },
+    { r: 61, g: 0, b: 0 },
+    { r: 139, g: 0, b: 0 },
+    { r: 255, g: 69, b: 0 },
+    { r: 255, g: 165, b: 0 },
+    { r: 255, g: 255, b: 0 },
+  ],
+  rainbow: [
+    { r: 255, g: 0, b: 0 },
+    { r: 255, g: 127, b: 0 },
+    { r: 255, g: 255, b: 0 },
+    { r: 0, g: 255, b: 0 },
+    { r: 0, g: 0, b: 255 },
+    { r: 75, g: 0, b: 130 },
+    { r: 148, g: 0, b: 211 },
+  ],
+  grayscale: [
+    { r: 0, g: 0, b: 0 },
+    { r: 26, g: 26, b: 26 },
+    { r: 77, g: 77, b: 77 },
+    { r: 128, g: 128, b: 128 },
+    { r: 179, g: 179, b: 179 },
+    { r: 255, g: 255, b: 255 },
+  ],
+  aurora: [
+    { r: 0, g: 20, b: 40 },
+    { r: 0, g: 100, b: 100 },
+    { r: 0, g: 200, b: 150 },
+    { r: 100, g: 255, b: 200 },
+    { r: 150, g: 100, b: 255 },
+    { r: 255, g: 50, b: 150 },
+  ],
+  neon: [
+    { r: 10, g: 10, b: 10 },
+    { r: 255, g: 0, b: 255 },
+    { r: 0, g: 255, b: 255 },
+    { r: 255, g: 255, b: 0 },
+    { r: 0, g: 255, b: 0 },
+    { r: 255, g: 0, b: 0 },
+  ],
+  vintage: [
+    { r: 44, g: 25, b: 14 },
+    { r: 101, g: 67, b: 33 },
+    { r: 152, g: 118, b: 84 },
+    { r: 205, g: 173, b: 122 },
+    { r: 237, g: 214, b: 176 },
+    { r: 255, g: 245, b: 225 },
+  ],
+  cyberpunk: [
+    { r: 0, g: 0, b: 0 },
+    { r: 138, g: 0, b: 184 },
+    { r: 255, g: 0, b: 127 },
+    { r: 0, g: 255, b: 255 },
+    { r: 255, g: 215, b: 0 },
+    { r: 255, g: 255, b: 255 },
+  ],
+  lava: [
+    { r: 0, g: 0, b: 0 },
+    { r: 128, g: 0, b: 0 },
+    { r: 255, g: 69, b: 0 },
+    { r: 255, g: 140, b: 0 },
+    { r: 255, g: 215, b: 0 },
+    { r: 255, g: 255, b: 200 },
+  ],
+  ice: [
+    { r: 0, g: 0, b: 20 },
+    { r: 20, g: 40, b: 80 },
+    { r: 50, g: 100, b: 150 },
+    { r: 100, g: 180, b: 220 },
+    { r: 180, g: 230, b: 255 },
+    { r: 240, g: 250, b: 255 },
+  ],
+  toxic: [
+    { r: 0, g: 0, b: 0 },
+    { r: 20, g: 50, b: 0 },
+    { r: 50, g: 100, b: 0 },
+    { r: 100, g: 200, b: 0 },
+    { r: 150, g: 255, b: 0 },
+    { r: 200, g: 255, b: 100 },
+  ],
+  sakura: [
+    { r: 50, g: 20, b: 40 },
+    { r: 120, g: 60, b: 90 },
+    { r: 200, g: 120, b: 150 },
+    { r: 255, g: 180, b: 200 },
+    { r: 255, g: 220, b: 230 },
+    { r: 255, g: 240, b: 245 },
+  ],
+  deepspace: [
+    { r: 0, g: 0, b: 10 },
+    { r: 20, g: 0, b: 40 },
+    { r: 60, g: 20, b: 100 },
+    { r: 100, g: 50, b: 150 },
+    { r: 150, g: 100, b: 200 },
+    { r: 200, g: 150, b: 255 },
+  ],
+  copper: [
+    { r: 20, g: 10, b: 0 },
+    { r: 80, g: 40, b: 20 },
+    { r: 150, g: 90, b: 50 },
+    { r: 200, g: 130, b: 80 },
+    { r: 230, g: 170, b: 120 },
+    { r: 255, g: 200, b: 150 },
+  ],
+  tropical: [
+    { r: 0, g: 40, b: 60 },
+    { r: 0, g: 120, b: 140 },
+    { r: 255, g: 180, b: 0 },
+    { r: 255, g: 100, b: 100 },
+    { r: 200, g: 50, b: 150 },
+    { r: 255, g: 220, b: 100 },
+  ],
+  midnight: [
+    { r: 10, g: 10, b: 30 },
+    { r: 20, g: 20, b: 60 },
+    { r: 40, g: 50, b: 100 },
+    { r: 80, g: 90, b: 140 },
+    { r: 120, g: 130, b: 180 },
+    { r: 180, g: 190, b: 230 },
+  ],
+  desert: [
+    { r: 60, g: 40, b: 20 },
+    { r: 120, g: 80, b: 40 },
+    { r: 180, g: 120, b: 60 },
+    { r: 220, g: 160, b: 100 },
+    { r: 240, g: 200, b: 140 },
+    { r: 255, g: 230, b: 180 },
+  ],
+  candy: [
+    { r: 100, g: 0, b: 100 },
+    { r: 200, g: 0, b: 150 },
+    { r: 255, g: 100, b: 200 },
+    { r: 255, g: 150, b: 255 },
+    { r: 200, g: 200, b: 255 },
+    { r: 255, g: 255, b: 255 },
+  ],
+  earth: [
+    { r: 40, g: 50, b: 30 },
+    { r: 80, g: 90, b: 50 },
+    { r: 120, g: 130, b: 80 },
+    { r: 160, g: 150, b: 100 },
+    { r: 200, g: 180, b: 140 },
+    { r: 230, g: 210, b: 180 },
+  ],
+  plasma: [
+    { r: 13, g: 8, b: 135 },
+    { r: 84, g: 2, b: 163 },
+    { r: 139, g: 10, b: 165 },
+    { r: 185, g: 50, b: 137 },
+    { r: 219, g: 92, b: 104 },
+    { r: 244, g: 143, b: 66 },
+    { r: 253, g: 219, b: 39 },
+  ],
+  viridis: [
+    { r: 68, g: 1, b: 84 },
+    { r: 59, g: 82, b: 139 },
+    { r: 33, g: 145, b: 140 },
+    { r: 94, g: 201, b: 98 },
+    { r: 253, g: 231, b: 37 },
+  ],
 };
 
 export class ColorPalette {
-    constructor(paletteName = 'rainbow') {
-        this.setPalette(paletteName);
+  constructor(paletteName = 'rainbow') {
+    this.setPalette(paletteName);
+  }
+
+  setPalette(paletteName) {
+    this.palette = palettes[paletteName] || palettes.rainbow;
+    this.paletteName = paletteName;
+  }
+
+  // Linear interpolation between two colors
+  lerp(color1, color2, t) {
+    return {
+      r: Math.round(color1.r + (color2.r - color1.r) * t),
+      g: Math.round(color1.g + (color2.g - color1.g) * t),
+      b: Math.round(color1.b + (color2.b - color1.b) * t),
+    };
+  }
+
+  // Get color for a normalized value (0-1)
+  getColor(value) {
+    if (value <= 0) return this.palette[0];
+    if (value >= 1) return this.palette[this.palette.length - 1];
+
+    const scaledValue = value * (this.palette.length - 1);
+    const index = Math.floor(scaledValue);
+    const t = scaledValue - index;
+
+    if (index >= this.palette.length - 1) {
+      return this.palette[this.palette.length - 1];
     }
 
-    setPalette(paletteName) {
-        this.palette = palettes[paletteName] || palettes.rainbow;
-        this.paletteName = paletteName;
+    return this.lerp(this.palette[index], this.palette[index + 1], t);
+  }
+
+  // Get color with smooth coloring for fractals
+  getSmoothColor(iterations, maxIterations, smoothValue = 0) {
+    if (iterations >= maxIterations) {
+      return { r: 0, g: 0, b: 0 }; // Inside the set - black
     }
 
-    // Linear interpolation between two colors
-    lerp(color1, color2, t) {
-        return {
-            r: Math.round(color1.r + (color2.r - color1.r) * t),
-            g: Math.round(color1.g + (color2.g - color1.g) * t),
-            b: Math.round(color1.b + (color2.b - color1.b) * t)
-        };
-    }
-
-    // Get color for a normalized value (0-1)
-    getColor(value) {
-        if (value <= 0) return this.palette[0];
-        if (value >= 1) return this.palette[this.palette.length - 1];
-
-        const scaledValue = value * (this.palette.length - 1);
-        const index = Math.floor(scaledValue);
-        const t = scaledValue - index;
-
-        if (index >= this.palette.length - 1) {
-            return this.palette[this.palette.length - 1];
-        }
-
-        return this.lerp(this.palette[index], this.palette[index + 1], t);
-    }
-
-    // Get color with smooth coloring for fractals
-    getSmoothColor(iterations, maxIterations, smoothValue = 0) {
-        if (iterations >= maxIterations) {
-            return { r: 0, g: 0, b: 0 }; // Inside the set - black
-        }
-
-        // Smooth coloring using the smooth value
-        const normalized = (iterations + 1 - smoothValue) / maxIterations;
-        return this.getColor(normalized);
-    }
+    // Smooth coloring using the smooth value
+    const normalized = (iterations + 1 - smoothValue) / maxIterations;
+    return this.getColor(normalized);
+  }
 }
